30fb64a5cd2d6bcb5cbf96911efd0e901c524919
Gaizgrol <gabrielizotongo@gmail.com>
2023-03-27 20:47:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Take Home Assignment',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
